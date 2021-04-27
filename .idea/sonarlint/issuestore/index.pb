956ec811b646ce161608f81e1cf3080ced85481f
TheTupok <ter.tolya@mail.ru>
2021-04-27 23:32:35 +0300

    add postgresql Post models

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -2,3 +2,15 @@
 :
 
 .gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
+>
+events/apps.py,9\2\92982d9eda0e00f14664ddf1e7765fd0bfa190c9
+?
+events/admin.py,2\3\236af2ecaec47c4a092c67690312b83917b3ff7f
+@
+events/models.py,b\4\b43f3d31bab1e3c3aaaefe017b1f7bd74d6670ea
+E
+myawesomeblog/urls.py,0\8\0837166e68145352b39a826cfad40b5777b8c3bf
+Q
+!events/migrations/0001_initial.py,a\b\abf1f1e326e96952ce8108f769e6d7301766c3fc
+I
+myawesomeblog/settings.py,5\c\5cc237fb1522a4dad7d7dd146d83384d97cca330
--- myawesomeblog/settings.py
+++ myawesomeblog/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'events.apps.EventsConfig',
+    'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'myawesomeblog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myawesomeblogdb',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
